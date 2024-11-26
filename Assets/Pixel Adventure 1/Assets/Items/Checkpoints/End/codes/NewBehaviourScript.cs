fef4311f68c69ff624f6be6cc0fd08a50b24b430
bertday000 <shoniazuka06@gmail.com>
2024-11-26 19:46:53 +0400

    init

---
+++ Assets/Pixel Adventure 1/Assets/Items/Checkpoints/End/codes/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string MyName = "Zuka";
+        string LastName = "shonia";
+        string MyHobby = "building with legos";
+        int MyAge = 15;
+
+        Debug.Log("Hello my name is " + MyName + " and my last name is " + LastName + " i am " + MyAge + " and my hobby is " + MyHobby);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
